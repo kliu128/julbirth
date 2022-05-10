31eb693ccc10baf6681a19ee898dd83316868046
Kevin Liu <kevin@kliu.io>
2022-05-09 23:12:36 -0700

    woot

--- netlify/functions/complete/complete.ts
+++ netlify/functions/complete/complete.ts
@@ -1,12 +1,26 @@
-import { Handler } from '@netlify/functions'
+import { Handler } from "@netlify/functions";
+import { Configuration, OpenAIApi } from "openai";
 
 export const handler: Handler = async (event, context) => {
-  const { name = 'stranger' } = event.queryStringParameters
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const completion = await openai.createCompletionFromModel({
+    model: "curie:ft-personal-2022-05-10-05-46-23",
+    prompt: "give julian a birthday message",
+  });
+
+  //@ts-ignore
+  console.log(completion.data.choices[0].text);
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello, ${name}!`,
-    }),
-  }
-}
+    //@ts-ignore
+    body: completion.data.choices[0].text,
+  };
+};
+
+//@ts-ignore
+handler(undefined, undefined);
